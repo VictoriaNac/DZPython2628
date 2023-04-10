e5273851ae4d2d03890fa7b89025fda129e59f51
Viktoria <android1986mir@rambler.ru>
2023-04-10 09:03:11 +0300

    6

--- 6.py
+++ 6.py
@@ -14,6 +14,6 @@ s = str(input())
 sum1=int(s[0])+int(s[1])+int(s[2])
 sum2=int(s[3])+int(s[4])+int(s[5])
 if sum1==sum2:
-  print('Счастливый билет')
+  print('Yes')
 else:
-  print('Обычный билет')
+  print('No')
